01a0e0377d66e69ac6d0d5420c56bb954dccc2e6
Todd Libby <toddlibby@protonmail.com>
2024-06-23 10:29:59 -0700

    Working through more example files and tutorials

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    @Test
+    void shouldCalculateAverageOfPositiveNumbers() {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        double actualAverage = Calculator.average(numbers);
+
+        double expectedAverage = 3.0;
+        assertEquals(expectedAverage, actualAverage);
+    }
+
+    @Test
+    void shouldReturnZeroAverageForEmptyArray() {
+        int[] numbers = {};
+
+        double actualAverage = Calculator.average(numbers);
+
+        double expectedAverage = 0;
+        assertEquals(expectedAverage, actualAverage);
+    }
+}
